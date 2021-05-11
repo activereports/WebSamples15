764c2dc4b75fad1127db8d4431ca322d9a9d58a0
Maxim Voronkov <maxim.voronkov@grapecity.com>
2021-05-11 15:31:55 +0700

    Case AR-26362. Added new sample
    The new sample WebDesigner_CustomDataProviders demonstrates 3 features:
    1. Usage of OData as custom data provider.
    In order to test this feature go to the data tab, add new Data Source, select "C1 ODATA Provider" and enter Connection String like "Url=https://services.odata.org/V4/Northwind/Northwind.svc".
    Now you can create DataSets and attach to the report fields.
    
    Also you can load the sample report "InvoiceOData.rdlx" that uses OData DataSource already.
    
    2. Usage of SQLite as custom data provider.
    In order to test this feature go to the data tab, add new Data Source, select "SQLite Provider" and enter Connection String like "Url=c:\temp\Northwind.sqlite".
    The new sample contains the sample SQlite database.
    
    Also you can load the sample report "ListSqlite.rdlx" that uses Sqlite DataSource already.
    
    3. Usage of custom OData DataSets.
    You can find its definitions at the folder "datasets".
    In order to add it go to the data tab and simply add new DataSet.
    
    p.s. if you run into a problem with OData data provider, you might need to register and install trial version of the ComponentOne's "Service Components": https://www.grapecity.com/componentone/download

---
+++ WebDesignerSamples/WebDesigner_CustomDataProviders/Implementation/ODataDataSets.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using WebDesignerCustomDataProviders.Services;
+
+namespace WebDesignerCustomDataProviders.Implementation
+{
+	internal class ODataDataSets : IDataSetsService
+	{
+		private readonly DirectoryInfo _rootFolder;
+
+		public ODataDataSets(DirectoryInfo rootFolder)
+		{
+			_rootFolder = rootFolder;
+		}
+
+		private static string DataSetExtension = ".json";
+
+		public object[] GetDataSetsList()
+		{
+			var dataSetsList = _rootFolder.GetFiles("*.*", SearchOption.TopDirectoryOnly)
+				.Select(fileInfo => fileInfo.Name)
+				.Where(x => string.Equals(Path.GetExtension(x), DataSetExtension,
+					StringComparison.InvariantCultureIgnoreCase))
+				.Select(name => new
+				{
+					Id = name,
+					Name = name.Substring(0, name.Length - DataSetExtension.Length),
+				})
+				.ToArray();
+			return dataSetsList;
+		}
+
+		public object GetDataSet(string id)
+		{
+			var name = Uri.UnescapeDataString(id);
+			var fullPath = Path.Combine(_rootFolder.FullName, name);
+
+			if (!File.Exists(fullPath)) throw new ArgumentException();
+			
+			// Read result template
+			var template = JObject.Parse(File.ReadAllText(fullPath));
+			
+			// Fetch OData url
+			var oDataUrl = template["ODataUrl"].ToString();
+
+			// Download and parse OData json
+			JObject parsed;;
+			using (var webClient = new WebClient())
+			{
+				webClient.Encoding = Encoding.UTF8;
+				var json = webClient.DownloadString(oDataUrl);
+
+				parsed = JObject.Parse(json);
+			}
+			
+			// Rename "value" to "Data"
+			parsed["Data"] = parsed["value"];
+			parsed.Remove("value");
+
+			var jsonData = JsonConvert.SerializeObject(parsed);
+
+			// Set data in the template
+			template["DataSource"]["ConnectionProperties"]["ConnectString"] = "jsonData=" +	jsonData;
+
+			return template.ToString();
+		}
+	}
+}
